f453b771ead71d8ed6169c6f5c5d6d0a1c38bd57
nikhilshaji17 <nikhilshaji17@gmail.com>
2025-10-29 17:18:13 +0400

    Added more JS notes

--- FEM/experiment/objects.js
+++ FEM/experiment/objects.js
@@ -16,3 +16,24 @@ console.log("-------------------We are in objects.js--------------------------")
 
 // dog.speak();
 // dog["speak"]();
+
+
+const cities = ["Abu Dhabi", "Dubai", "Hong Kong"];
+
+// for (let i = 0; i < cities.length; i++) {
+// 	console.log(cities[i]);
+// }
+
+// cities.forEach(function (city, index) {
+// 	console.log(city, "using forEach and function defined as argument", index);
+// });
+
+// function logCity(city) {
+// 	console.log(city, "using separately defined function");
+// }
+
+// cities.forEach(logCity);
+
+// cities.forEach((city)=>{
+// 	console.log(city);
+// })
